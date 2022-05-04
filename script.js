1ae230247cc93b28f54c29e21debeca9f7e96035
mosserryan <mosser.ryan.a@gmail.com>
2022-05-04 15:52:39 -0700

    Added PUT to cards, cleaned up some loose ends.

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
 let cardCount = 0;
+let cardBody;
+let cardImg;
+let currentMonsterId;
+let desc =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut nisi laboriosam voluptatem expedita tempora quidem quisquam quos, nostrum dicta sit obcaecati aspernatur distinctio beatae esse odit eveniet exercitationem! Voluptate, quam.";
+
+function toggleModal(selected) {
+  console.log(selected);
+  currentMonsterId = selected.children[0].getAttribute("id");
+  cardBody = selected.children[0].children[1];
+  cardImg = selected.children[0].children[0];
+  document
+    .getElementById("modal_img")
+    .setAttribute("src", cardImg.getAttribute("src"));
+  const [cardId, name, codeType] = cardBody.children;
+  console.log(cardId.innerText);
+  document.getElementById("modal_card_id").innerText = cardId.innerText;
+  document.getElementById("modal_name").innerText = name.innerText;
+  document.getElementById("modal_code_type").innerText = codeType.innerText;
+  document.getElementById("modal_desc").innerText = desc;
+  // document.getElementById("modal_photo").value = cardImg.getAttribute("src");
+
+  document.getElementById("monster_name").value = name.innerText;
+  document.getElementById("code_type").value = codeType.innerText;
+  document.getElementById("monster_description").value = desc;
+
+  const putURL = `https://monster-collector.herokuapp.com/monster/${currentMonsterId}`;
+  document.getElementById("modal_edit_form").setAttribute("action", putURL);
+}
+
+// refactor with time.
+function editMonster() {
+  console.log(currentMonsterId);
+  toggleHide();
+}
+
+function toggleHide() {
+  document.getElementById("input_forms").classList.remove("hidden");
+  document.getElementById("monster_info").classList.add("hidden");
+  document.getElementById("input_buttons").classList.remove("hidden");
+  document.getElementById("info_buttons").classList.add("hidden");
+}
 
 function displayFormData({ _id, name, isScary, owner }) {
   function capitalizeFirstLetter(name) {
@@ -58,6 +100,15 @@ function displayFormData({ _id, name, isScary, owner }) {
   const cardDiv = document.createElement("div");
   cardDiv.appendChild(card);
   cardDiv.classList.add("card_div");
+
+  cardDiv.setAttribute("data-toggle", "modal");
+  cardDiv.setAttribute("data-target", "#currentModal");
+  cardDiv.setAttribute("onclick", "toggleModal(this)");
+
+  // const editBtn = document.createElement("i");
+  //
+  // editBtn.classList.add("fa-solid", "fa-pen-to-square", "btn", "edit_icon");
+  // card.appendChild(editBtn);
 
   document.getElementById("cards_container").appendChild(cardDiv);
 }
